edaeab9cf8b70f6db06c26f6743640b1111eb386
DELL <veer2667@gmail.com>
2024-04-09 22:51:46 +0530

    Added unit tests for getDataPoint and getRatio method

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      final_price = (quote['top_ask']['price']+quote['top_bid']['price'])/2
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_ask']['price'], quote['top_bid']['price'], final_price))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +19,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertGreater(quote['top_bid']['price'],quote['top_ask']['price'])
 
 
   """ ------------ Add more unit tests ------------ """
+def test_getRatio_calculateRatio(self):
+  quotes = [
+    {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+     'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+    {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+     'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+  ]
+  prices = {}
+  for quote in quotes:
+    stock = quote[stock]
+    prices[stock] = getDataPoint(quote)[3]
+
+  expected_ratio = (119.2+120.48)/(121.68+117.87)
+  self.assertEqual(getRatio(prices['ABC'], prices['DEF']), expected_ratio)
 
 
 
